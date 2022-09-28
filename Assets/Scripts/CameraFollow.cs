41da6157aad23f81c1b2848310444a75ad94acd8
Pake Jackson <pjacks22@my.centennialcollege.ca>
2022-09-28 19:16:55 -0400

    Add camera script to follow player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform playerTransform;
+    public GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+            Vector3 temp = transform.position;
+            temp.x = playerTransform.position.x;
+            transform.position = temp;
+            temp.y = playerTransform.position.y;
+            transform.position = temp;
+    }
+}
