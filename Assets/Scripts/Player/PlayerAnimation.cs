b85db0b585a49f406379a5e643df6fca64db3970
Albert Stanica <nois3less@gmail.com>
2022-10-07 11:08:51 -0300

    Added Idle and Walk animations

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigidBody;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Update()
+    {
+        if(rigidBody.velocity.x < -0.2)
+        {
+            if(spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+        if(rigidBody.velocity.x > 0.2)
+        {
+            if(spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        anim.SetFloat("speed", Mathf.Abs(rigidBody.velocity.x));
+    }
+}
