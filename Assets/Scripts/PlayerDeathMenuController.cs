635275998be32f89c37775849bbbc8d654742f9d
Aidan <33384177+PhyrexianAgent@users.noreply.github.com>
2022-10-08 21:23:08 -0400

    Made Death Buttons Functional

--- Assets/Scripts/PlayerDeathMenuController.cs
+++ Assets/Scripts/PlayerDeathMenuController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeathMenuController : MonoBehaviour
 {
+    private const string MAIN_MENU_SCENE_NAME = "Main Menu";
 
     public static PlayerDeathMenuController instance;
 
+    [SerializeField] private string currentLevelName = "Level 1";
 
     public Animation anim;
 
@@ -19,5 +22,15 @@ public class PlayerDeathMenuController : MonoBehaviour
     {
         Debug.Log("playing");
         anim.Play();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(currentLevelName);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(MAIN_MENU_SCENE_NAME);
     }
 }
