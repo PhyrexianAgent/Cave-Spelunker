2a3eae7b75b853ce2b6a6630e75b0aa8ea564995
Pake Jackson <pjacks22@my.centennialcollege.ca>
2022-10-08 22:47:24 -0400

    Update QuitGameScript.cs

--- Assets/Scripts/QuitGameScript.cs
+++ Assets/Scripts/QuitGameScript.cs
@@ -7,6 +7,6 @@ public class QuitGameScript : MonoBehaviour
     public void WasPressed()
     {
         Application.Quit(); // this will quite the game (when not run from editor).
-        UnityEditor.EditorApplication.isPlaying = false; // Is used to quit game from editor. Can be removed if causes issues when compiled and ran.
+        //UnityEditor.EditorApplication.isPlaying = false; // Is used to quit game from editor. Can be removed if causes issues when compiled and ran.
     }
 }
